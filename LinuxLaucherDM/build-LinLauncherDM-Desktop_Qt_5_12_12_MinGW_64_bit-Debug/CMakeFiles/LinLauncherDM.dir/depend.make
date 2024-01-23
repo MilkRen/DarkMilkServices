99f83c96ec0431a62032b80bc6767bd6d16daa7a
ren <grangn20@gmail.com>
2024-01-23 21:22:54 +0300

    Удалил лишнее для QT

--- LinuxLaucherDM/build-LinLauncherDM-Desktop_Qt_5_12_12_MinGW_64_bit-Debug/CMakeFiles/LinLauncherDM.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for LinLauncherDM.
-# This may be replaced when dependencies are built.
--- LinuxLaucherDM/build-LinLauncherDM-Desktop_Qt_5_12_12_MinGW_64_bit-Debug/CMakeFiles/LinLauncherDM.dir/serverrequest.cpp.obj.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/LinLauncherDM.dir/serverrequest.cpp.obj: \
- C:\Users\rinat\Desktop\OtherLanguages\C_Cplus_plus\QT\LinLauncherDM\serverrequest.cpp \
- C:\Users\rinat\Desktop\OtherLanguages\C_Cplus_plus\QT\LinLauncherDM\serverrequest.h
