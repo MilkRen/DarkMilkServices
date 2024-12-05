c57ce870a6f7c92010855db8bdfc28231dab4391
Rinaz <grangn20@gmail.com>
2024-12-05 23:53:29 +0300

    Финал
    Всё, что успел

--- LinuxLaucherDM/LinLauncherDM/serverrequest.cpp
+++
@@ -1,6 +0,0 @@
-#include "serverrequest.h"
-
-ServerRequest::ServerRequest()
-{
-
-}
--- WebAppDM/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace WebAppDM.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
